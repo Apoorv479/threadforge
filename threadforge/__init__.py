18f0683e63fb5a67d30e9284abfe094ea1b78102
Apoorv Kumar Jha <apoorv172900@gmail.com>
2026-08-30 20:17:50 +0530

    Add imports and versioning to __init__.py

--- threadforge/__init__.py
+++ threadforge/__init__.py
@@ -1,1 +1,14 @@
+"""ThreadForge - lightweight concurrent job execution engine."""
 
+from .engine import ThreadForge
+from .queue import BackpressurePolicy
+from .task import Task, TaskState
+
+__version__ = "0.1.0"
+
+__all__ = [
+    "ThreadForge",
+    "Task",
+    "TaskState",
+    "BackpressurePolicy",
+]
